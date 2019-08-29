f890e8970c7abe1159dab1e8ddbf4414da7d794d
Aluno <aluno@ifpi.edu.br>
2019-08-29 20:02:53 -0300

    Craicao do editar

---
+++ aula/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=50)
+    matricula = models.IntegerField()
+    sexo = models.CharField(max_length=1)
