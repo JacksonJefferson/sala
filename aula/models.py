e8f172e7e3a7db2dd9de8085c3d198a8c2fe1610
Aluno <aluno@ifpi.edu.br>
2019-08-30 19:39:28 -0300

    Add str no modelo

--- aula/models.py
+++ aula/models.py
@@ -4,3 +4,6 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=50)
     matricula = models.IntegerField()
     sexo = models.CharField(max_length=1)
+    
+    def __str__(self):
+        return self.nome
