12662992bc29aea482bca3cca9fc9a11e8ea6868
Aluno <aluno@ifpi.edu.br>
2019-08-30 18:42:41 -0300

    Alteração do base

--- aula/urls.py
+++ aula/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('aluno/', views.home),
     path('aluno/show/<int:id>', views.aluno_show),
-    path('aluno/list', views.aluno_list),
+    path('aluno/list/', views.aluno_list),
     path('aluno/create/', views.create),
     path('aluno/<int:id>/', views.delete),
     path('aluno/editar/<int:id>', views.editar),
